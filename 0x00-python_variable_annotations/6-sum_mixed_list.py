f5841c35e03b77a96504584da4c7d8d07c0f262a
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2023-03-30 11:10:39 +0200

    pycodestyle checks

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Module contains function sum_mixed_list that takes a list of both floats and ints
+Module contains function sum_mixed_list that takes a list of floats and ints
 and returns the sum of all elements as a float
 """
 from typing import List, Union
@@ -8,7 +8,7 @@ from typing import List, Union
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
-    sum_mixed_list() -> float: type-annotated function takes union List (mxd_lst)
+    sum_mixed_list() -> float: type-annotated function takes union (mxd_lst)
     and returns the sum of all elements.
     """
     return float(sum(mxd_lst))
