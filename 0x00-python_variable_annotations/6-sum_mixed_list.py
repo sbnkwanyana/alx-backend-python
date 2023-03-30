eac9a175d180f345ba9476bd717d39ee528affd2
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2023-03-30 10:40:12 +0200

    swapped int and float to pass requirements

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -6,7 +6,7 @@ and returns the sum of all elements as a float
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
     sum_mixed_list() -> float: type-annotated function takes union List (mxd_lst)
     and returns the sum of all elements.
