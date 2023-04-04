aeedc37c6e69e5c68124043379207870226dfa09
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2023-04-04 23:03:33 +0200

    0. Async Generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Module contains function async_generator that yield returns to random values
+"""
+import asyncio
+import random
+
+
+async def async_generator():
+    """
+    function yield returns 10 random values after sleeping for 1 second
+    between each return
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.random() * 10
