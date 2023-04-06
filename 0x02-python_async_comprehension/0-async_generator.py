e60dda28904819b45cee3eac529ae9f9c8bdadb2
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2023-04-06 23:42:35 +0200

    imported generator type as a return type

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,9 +4,10 @@ Module contains function async_generator that yield returns to random values
 """
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator() -> float:
+async def async_generator() -> Generator[float, None, None]:
     """
     function yield returns 10 random values after sleeping for 1 second
     between each return
