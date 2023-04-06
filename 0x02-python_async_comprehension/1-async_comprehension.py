2c526aabb81a20b5b5ddff0fd16d685616287573
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2023-04-06 23:44:50 +0200

    returnList[float] and not List[int]

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -9,7 +9,7 @@ from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> List[int]:
+async def async_comprehension() -> List[float]:
     """
     Function uses async comprehension using async for to create and
     return a list of randomly generated values
